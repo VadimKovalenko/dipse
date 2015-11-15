d94c856f7458ae428be6334e975253f1e65344ed
Vadim Kovalenko <vadblaxmith@gmail.com>
2015-11-15 19:23:42 +0200

    First DIPSE pages commit

---
+++ scripts/common.js
@@ -0,0 +1,7 @@
+﻿$(window).load(function() {
+	$(".center-one-center").animated("fadeIn");
+	$(".top-left").animated("slideInLeft");
+	$(".center-one-right").animated("fadeIn");
+	$(".center-one-left").animated("fadeIn");
+	$(".top-right").animated("slideInRight");
+});
